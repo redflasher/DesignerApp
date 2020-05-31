54e4cd9520af635591cec2eb15efeff4ed8ddd02
Alexey Burlakov <redflasher@yandex.ru>
2020-05-31 23:21:40 +0300

    init

--- App.js
+++ App.js
@@ -1,19 +1,50 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+    Dimensions,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View,
+} from 'react-native';
+
+import Basic from './examples/basic';
+
+const componentMap = {
+    Basic,
+};
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+    const [mode, setMode] = useState('Basic');
+
+    const renderExample = () => {
+        const Component = componentMap[mode];
+        return <Component />;
+    };
+
+    return (
+        <View style={styles.container}>
+            {renderExample()}
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        backgroundColor: 'white',
+        flex: 1,
+    },
+    switchContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        marginVertical: 50,
+        flexWrap: 'wrap',
+    },
+    switch: {
+        alignItems: 'center',
+        borderWidth: 1,
+        borderColor: 'black',
+        marginVertical: 2,
+        paddingVertical: 10,
+        width: Dimensions.get('window').width / 3,
+    },
 });
